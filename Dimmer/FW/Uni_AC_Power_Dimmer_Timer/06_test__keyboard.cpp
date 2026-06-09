b1d288d93a61bde4455a51676ac05c36f2fc33e9
Bosko Mitrovic <bosko.mitrovic13@gmail.com>
2026-06-09 13:31:33 +0200

    Implemented some keyboard functions

--- Dimmer/FW/Uni_AC_Power_Dimmer_Timer/06_test__keyboard.cpp
+++ Dimmer/FW/Uni_AC_Power_Dimmer_Timer/06_test__keyboard.cpp
@@ -4,35 +4,7 @@
 
 #include "common.hpp"
 
-#include "avr_io_bitfields.h"
 #include "uart_stdio.hpp"
-
-
-struct bf_c {
-	//TODO
-	u8 r1 : 1;
-	u8 r2 : 1;
-	u8 r3 : 1;
-	u8 r4 :	1;
-	u8 c3 : 1;
-	u8 c2 : 1;
-	u8 c1 : 1;
-	u8 c0 : 1;
-};
-
-
-struct bf_d {
-	u8 rxd : 1;
-	u8 txd : 1;
-	u8 zc1 : 1;
-	u8 zc2 : 1;
-	u8 drv1 : 1;
-	u8 drv2 : 1;
-	u8 r0 : 1;
-	u8 drv3 : 1;
-};
-#define o_buzz (*((volatile bf_d*)(&PORTD)))
-
 
 int main(void) {
 	screen.odd_0 = wide_bool(DDR_OUT);
@@ -42,55 +14,29 @@ int main(void) {
 
 	//screen.rgb0 = screen.rgb1 = WHITE;
 
-	DDRC = bf_to_u8<bf_c>({
-			.r1 = 0,
-			.r2 = 0,
-			.r3 = 0,
-			.r4 = 0,
-			.c3 = 1,
-			.c2 = 1,
-			.c1 = 1,
-			.c0 = 1
-		});
+	ddrc.b0 = 0;	//DDR_IN
+	ddrc.b1 = 0;
+	ddrc.b2 = 0;
+	ddrc.b3 = 0;	
+	ddrc.b4 = 1;	//DDR_OUT
+	ddrc.b5 = 1;
+	ddrc.b6 = 1;
+	ddrc.b7 = 1;
 
-	PORTC = bf_to_u8<bf_c>({
-			.r1 = 1,
-			.r2 = 1,
-			.r3 = 1,
-			.r4 = 1,
-			.c3 = 0,
-			.c2 = 0,
-			.c1 = 0,
-			.c0 = 0
-		});
+	// pull-ups on rows
+	portc.b0 = 1;
+	portc.b1 = 1;
+	portc.b2 = 1;
+	portc.b3 = 1;
 
-	// 0 - in
-	// 1 - out
-	DDRD = bf_to_u8<bf_d>({
-		.rxd = 0,
-		.txd = 1,
-		.zc1 = 0,
-		.zc2 = 0,
-		.drv1 = 1,
-		.drv2 = 1,
-		.r0 = 0,
-		.drv3 = 1
-	});
-	// For ins:
-	// 1 - pull up
-	PORTD = bf_to_u8<bf_d>({
-		.rxd = 0,
-		.txd = 1,
-		.zc1 = 0,
-		.zc2 = 0,
-		.drv1 = 0,
-		.drv2 = 0,
-		.r0 = 1,
-		.drv3 = 0
-	});
+	// columns HIGH
+	portc.b4 = 1;
+	portc.b5 = 1;
+	portc.b6 = 1;
+	portc.b7 = 1;
 
-	DDRD &= ~(1 << 6);   // input
-	PORTD |= (1 << 6);   // pull-up
+	ddrd.b6 = 0;
+	portd.b6 = 1;	//PORT_IN_PULL_UP
 
 	uart_stdio_non_blocking<100> _u;
 	printf("Hello!\n\r");
@@ -104,30 +50,33 @@ int main(void) {
 
 		for (int col = 0; col < 4; col++) {
 
-        // 1. all columns HIGH (PC4–PC7)
-        uint8_t base = 0x0F;        // rows pull-up (PC0–PC3 = 1)
-		uint8_t cols = 0xF0;        // all columns HIGH
+			// all columns HIGH
+			portc.b4 = 1;
+			portc.b5 = 1;
+			portc.b6 = 1;
+			portc.b7 = 1;
 
-		cols &= ~(1 << (7 - col)); // FIXED order
+			// set one column LOW
+			switch (col) {
+				case 0: portc.b7 = 0; break; // C0
+				case 1: portc.b6 = 0; break; // C1
+				case 2: portc.b5 = 0; break; // C2
+				case 3: portc.b4 = 0; break; // C3
+			}
 
-		PORTC = base | cols;
-        _delay_us(5);
+			_delay_us(5);
 
-        uint8_t pc = PINC;
-        uint8_t pd = PIND;
+			// R0 (PD6)
+			if (!pind.b6) {
+				printf("R0 C%d\n\r", col);
+			}
 
-        // --- R0 (PD6) ---
-        if (!(pd & (1 << 6))) {
-            printf("R0 C%d\n\r", col);
-        }
-
-        // --- R1–R4 (PC0–PC3) ---
-        for (int row = 0; row < 4; row++) {
-            if (!(pc & (1 << row))) {
-                printf("R%d C%d\n\r", row + 1, col);
-            }
-        }
-    }
+			// R1–R4 (PC0–PC3)
+			if (!pinc.b0) printf("R1 C%d\n\r", col);
+			if (!pinc.b1) printf("R2 C%d\n\r", col);
+			if (!pinc.b2) printf("R3 C%d\n\r", col);
+			if (!pinc.b3) printf("R4 C%d\n\r", col);
+		}
 
 	} // while
 
